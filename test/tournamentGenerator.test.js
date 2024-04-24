ed20f8b6c9161ced07fb658535e127289c231993
antoine <antoine.verlycl@gmail.com>
2024-04-24 20:32:01 +0200

    generate teams with players length equal to x * 12

--- test/tournamentGenerator.test.js
+++ test/tournamentGenerator.test.js
@@ -3,5 +3,13 @@ import TournamentGenerator from '../src/tournamentGenerator.js';
 import { expect } from 'chai';
 
 describe('TournamentGenerator', () => {
+    const totalPlayers = 2 * 12;
+    let players = [];
+    for (let i = 0; i < totalPlayers; i++) {
+        players.push(`player${i + 1}`);
+    }
+    const teamGenerator = new TeamGenerator(players);
+    teamGenerator.generateTeams(players);
+    const teams = teamGenerator.getTeams();
 
 });
