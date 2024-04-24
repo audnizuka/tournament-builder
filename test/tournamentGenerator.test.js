7039e7ec1f59ba6a2be9f666e9fa2e2092f8e08b
antoine <antoine.verlycl@gmail.com>
2024-04-24 20:31:03 +0200

    create file tournamentGenerator.test and import necessary classes : teamGenerator, tournaÃmentGenerator and chai module

---
+++ test/tournamentGenerator.test.js
@@ -0,0 +1,7 @@
+import TeamGenerator from "../src/teamGenerator.js";
+import TournamentGenerator from '../src/tournamentGenerator.js';
+import { expect } from 'chai';
+
+describe('TournamentGenerator', () => {
+
+});
