9a474b27b7cae017cfed63a6f48c00e87eb8a73c
antoine <antoine.verlycl@gmail.com>
2024-04-24 21:06:05 +0200

    create failing test 'players who join the generator together should be in the same team and teams should be equals'

--- test/teamGenerator.test.js
+++ test/teamGenerator.test.js
@@ -3,7 +3,7 @@ import { expect } from "chai";
 
 describe("testing teamGenerator class", () => {
     let teamGenerator;
-    const players = ['player1', 'player2', 'player3', 'player4', 'player5', 'player6'];
+    const players = ['player1', 'player2', 'player3', 'player4', 'player5', 'player6', ['player7', 'player8']];
 
     beforeEach(() => {
         teamGenerator = new TeamGenerator(players);
@@ -12,6 +12,7 @@ describe("testing teamGenerator class", () => {
     it("should generate 2 teams", () => {
         teamGenerator.generateTeams();
         const teams = teamGenerator.getTeams();
+        console.log(teams)
         expect(teams).to.have.lengthOf(2);
     });
 
@@ -20,6 +21,18 @@ describe("testing teamGenerator class", () => {
         const teams = teamGenerator.getTeams();
         teams.forEach(team => {
             expect(team.players).to.have.lengthOf(3);
+        });
+    });
+
+    it("players who join the generator together should be in the same team and teams should be equals", () => {
+        teamGenerator.generateTeams();
+        const teams = teamGenerator.getTeams();
+        teams.forEach(team => {
+            console.log(team);
+            expect(team.players).to.have.lengthOf(teamGenerator.playersPerTeam);
+            if (team.players.includes('player7')) {
+                expect(team.players).to.include('player8');
+            }
         });
     });
 });
